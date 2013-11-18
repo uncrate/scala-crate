52a46773a69213402a98d324023f5552b7e6de51
John Leacox <johnlcox@gmail.com>
2013-11-17 18:38:04 -0600

    Add OS X support

--- metadata.rb
+++ metadata.rb
@@ -9,8 +9,10 @@ version          '0.1.0'
   debian
   centos
   redhat
+  mac_os_x
 }.each do |os|
   supports os
 end
 
 depends 'uncrate_base'
+depends 'homebrew'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,23 +1,30 @@
-scala_tar_filename = File.basename(node['scala']['download_url'])
-scala_tar_filepath = "#{Chef::Config['file_cache_path']}/#{scala_tar_filename}"
-scala_extract_path = "#{Chef::Config['file_cache_path']}/scala/#{node['scala']['checksum']}"
-scala_install_path = "#{node['scala']['install_path']}/#{node['scala']['install_dir']}"
+case node[:platform]
+when "centos", "redhat", "debian", "ubuntu"
+  scala_tar_filename = File.basename(node['scala']['download_url'])
+  scala_tar_filepath = "#{Chef::Config['file_cache_path']}/#{scala_tar_filename}"
+  scala_extract_path = "#{Chef::Config['file_cache_path']}/scala/#{node['scala']['checksum']}"
+  scala_install_path = "#{node['scala']['install_path']}/#{node['scala']['install_dir']}"
 
-include_recipe "ark"
+  include_recipe "ark"
 
-ark node['scala']['install_dir'] do
-  url node['scala']['download_url']
-  path node['scala']['install_path']
-  checksum node['scala']['checksum']
-  mode 0755
-  action :put
-end
+  ark node['scala']['install_dir'] do
+    url node['scala']['download_url']
+    path node['scala']['install_path']
+    checksum node['scala']['checksum']
+    mode 0755
+    action :put
+  end
 
-link "#{node['scala']['install_path']}/#{node['scala']['symlink_dir']}" do
-  to scala_install_path
-end
+  link "#{node['scala']['install_path']}/#{node['scala']['symlink_dir']}" do
+    to scala_install_path
+  end
 
-template "#{node['uncrate']['envdir']}/scala.sh" do
-  source "scalaenv.sh.erb"
-  mode "0755"
+  template "#{node['uncrate']['envdir']}/scala.sh" do
+    source "scalaenv.sh.erb"
+    mode "0755"
+  end
+when "mac_os_x"
+  package 'scala' do
+      action :install
+    end
 end
